ea3437a42d567be38cc387ddb90e2f4272aa027d
HP <nishijayaraj@gmail.com>
2024-05-27 22:55:14 +0530

    basic-concepts : Updated Profile.jsx

--- basic-concept/src/components/Profile.jsx
+++ basic-concept/src/components/Profile.jsx
@@ -1,66 +1,67 @@
 
-import {getImageUrl} from '../utils'
+import { getImageUrl } from '../utils'
 
 
-export function Avatar({person, size}){  // This is object destructuring
-   return(
+export function Avatar({ person, size }) {  // This is object destructuring
+  return (
     <>
-        <img  src={getImageUrl(person)} width={size} height={size} alt={person.name} />
+      <img src={getImageUrl(person)} width={size} height={size} alt={person.name} />
     </>
-   );
+  );
 }
 
-export  function Card({children}){
-    return(
+export function Card({ children }) {
+  return (
     <>
-      <Avatar person={{ 
-          name: 'Katsuko Saruhashi', 
-          imageId: 'YfeOqp2'
-        }} size={100}/>
-      
+      <Avatar person={{
+        name: 'Katsuko Saruhashi',
+        imageId: 'YfeOqp2'
+      }} size={100} />
+
       <Avatar
         size={80}
         person={{
-          name: 'Aklilu Lemma', 
+          name: 'Aklilu Lemma',
           imageId: 'OKS67lh'
         }} />
 
       <Avatar
         size={50}
-        person={{ 
+        person={{
           name: 'Lin Lanying',
           imageId: '1bX5QH6'
         }} />
-    </> 
-    )
+    </>
+  )
 }
 
 
-export default function Profile(){
-    return(
-        <><Card/>
-        </Card</>
-        /*
+export default function Profile() {
+  return (
     <>
-      Avatar person={{ 
-          name: 'Katsuko Saruhashi', 
-          imageId: 'YfeOqp2'
-        }} size={100}/>
-      
-      <Avatar
-        size={80}
-        person={{
-          name: 'Aklilu Lemma', 
-          imageId: 'OKS67lh'
-        }} />
+      <Card />
+    </>
+    /*
+<>
+  <Avatar person={{ 
+      name: 'Katsuko Saruhashi', 
+      imageId: 'YfeOqp2'
+    }} size={100}/>
+  
+  <Avatar
+    size={80}
+    person={{
+      name: 'Aklilu Lemma', 
+      imageId: 'OKS67lh'
+    }} />
 
-      <Avatar
-        size={50}
-        person={{ 
-          name: 'Lin Lanying',
-          imageId: '1bX5QH6'
-        }} /> 
-    </> 
-    */
-    )
+  <Avatar
+    size={50}
+    person={{ 
+      name: 'Lin Lanying',
+      imageId: '1bX5QH6'
+    }} /> 
+</> 
+*/
+  )
 }
