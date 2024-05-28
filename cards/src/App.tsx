f482301811a3a10989b1d7bdcddbaca05f300ba9
HP <nishijayaraj@gmail.com>
2024-05-28 21:53:49 +0530

    Cards : displayed image cards

--- cards/src/App.tsx
+++ cards/src/App.tsx
@@ -1,34 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import felix from "./images/felix.png"
+import mrwhiskerson from "./images/mr-whiskerson.png"
+import phoneicon from "./images/phone-icon.png"
+import mailicon from "./images/mail-icon.png"
+import fluffykins from "./images/fluffykins.png"
+import pumpkin from "./images/pumpkin.png"
+
+/* Challenge:
+
+- Create a Contact.js component in another file
+- Move one of the contact card divs below into that file
+- import and render 4 instances of that contact card
+    - Think ahead: what's the problem with doing it this way?
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="contacts">
+
+      <div className="contact-card">
+        <img src={mrwhiskerson} />
+        <h3>Mr. Whiskerson</h3>
+        <div className="info-group">
+          <img src={phoneicon} />
+          <p>(212) 555-1234</p>
+        </div>
+        <div className="info-group">
+          <img src={mailicon} />
+          <p>mr.whiskaz@catnap.meow</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="contact-card">
+        <img src={fluffykins} />
+        <h3>Fluffykins</h3>
+        <div className="info-group">
+          <img src={phoneicon} />
+          <p>(212) 555-2345</p>
+        </div>
+        <div className="info-group">
+          <img src={mailicon} />
+          <p>fluff@me.com</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="contact-card">
+        <img src={felix} />
+        <h3>Felix</h3>
+        <div className="info-group">
+          <img src={phoneicon} />
+          <p>(212) 555-4567</p>
+        </div>
+        <div className="info-group">
+          <img src={mailicon} />
+          <p>thecat@hotmail.com</p>
+        </div>
+      </div>
+
+      <div className="contact-card">
+        <img src={pumpkin} />
+        <h3>Pumpkin</h3>
+        <div className="info-group">
+          <img src={phoneicon} />
+          <p>(0800) CAT KING</p>
+        </div>
+        <div className="info-group">
+          <img src={mailicon} />
+          <p>pumpkin@scrimba.com</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
